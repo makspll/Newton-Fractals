ed36c7cf9d274fc2533cd7b10c666f67d66724d2
Gedes <makspl17@gmail.com>
2018-11-23 04:22:54 +0000

    minor fixes

--- main.hs
+++ main.hs
@@ -29,18 +29,19 @@ main = do
 
 main = do
        getNumCapabilities >>= setNumCapabilities
-       putStrLn "Maks & Michal 2k18 >>= with love >> we present you with..."
-       putStrLn "----------------------------------------------------------"
-       putStrLn "  __                _        _     _     _ _  _ _ _ _ _   "
+       putStrLn "Maks & Michal 2k18 >>= with love >> we present you with...  "
+       putStrLn "----------------------------------------------------------  "
+       putStrLn "  __                _        _     _     _ _  _ _ _ _ _     "
        putStrLn " / _|              | |      | |    \\ \\  |  _||  _|_   _|  "
        putStrLn "| |_ _ __ __ _  ___| |_ __ _| |     \\ \\ | |_ | |_  | |    "
        putStrLn "|  _| '__/ _` |/ __| __/ _` | |     /\\ \\|  _||  _| | |    "
        putStrLn "| | | | | (_| | (__| || (_| | |    / /\\ \\ |_ | |_  | |    "
        putStrLn "|_| |_|  \\__,_|\\___|\\__\\__,_|_|   /_/  \\_\\__||___| |_|    "
-       putStrLn "----------------------------------------------------------"
-       putStrLn "   the ultimate newton method fractal yeeting software ©  "
-       putStrLn "----------------------------------------------------------"
-       putStrLn "To begin the ultimate yeet experience, enter no.of frames:"
+       putStrLn "----------------------------------------------------------  "
+       putStrLn "   the ultimate newton method fractal yeeting software ©    "
+       putStrLn "----------------------------------------------------------  "
+       putStrLn "       ...make sure you have read the user guide...         "
+       putStrLn "To begin the ultimate yeet experience, enter no.of frames:  "
        n <- getLine
        let nV = read n
        fs <- getFS
